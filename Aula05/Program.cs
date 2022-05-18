fa5370e79a952f39f759cf2640cf4eb304db3710
heber <heberhenrique@gmail.com>
2022-05-18 15:21:26 -0300

    subindo conteúdo da aula extra

---
+++ Aula05/Program.cs
@@ -0,0 +1,155 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("=====Aula 05!=======");
+// structs
+// metod
+// Serialization
+// Json
+// testes
+// tratamento de exceção
+const string caminhoArquivo = $"/users/heberhenriquedasilva/desktop/cliente.txt";
+
+var cliente = new DadosCliente();
+cliente.Nome = "Heber Henrique da Silva";
+cliente.DataNascimento = new DateTime(1988,8,21);
+cliente.EstadoCivil = EstadoCivil.Solteiro;
+cliente.Cpf = "388.241.050-76";
+
+var jsonStr = JsonSerializer.Serialize(cliente);
+
+using (var file = File.Open(caminhoArquivo, FileMode.OpenOrCreate))
+{
+    byte[] dados = new UTF8Encoding(true).GetBytes(jsonStr);
+    file.Write(dados, 0, dados.Length);
+
+    file.Flush();
+}
+
+var fileStr = File.ReadAllText(caminhoArquivo);
+
+var novoCliente = JsonSerializer.Deserialize<DadosCliente>(fileStr);
+
+Console.WriteLine(Marshal.SizeOf(novoCliente));
+Console.WriteLine(Marshal.SizeOf(new DadosCliente()));
+Console.WriteLine(Marshal.SizeOf(new DadosEmpresa()));
+Console.WriteLine(Marshal.SizeOf(new DadosEmpresaBolsa()));
+
+Console.WriteLine($"Nome cliente: {novoCliente.Nome}");
+Console.WriteLine($"Data de Nascimento: {novoCliente.DataNascimento}");
+Console.WriteLine($"Estado Civil: {novoCliente.EstadoCivil}");
+Console.WriteLine($"Cpf: {novoCliente.Cpf}");
+
+(string Nome, DateTime DataNascimento) cliente2 = ("Henrique", new DateTime(1988, 8, 21));
+
+var listaTupla = new List<(string Nome, DateTime DataNascimento)>();
+listaTupla.Add(("Silva", new DateTime(1988, 8, 21)));
+
+//ObjectCache cache = MemoryCache.Default;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+
+PrintLine();
+PrintRow("Nome", "Data Nascimento", "Estado Civil", "CPF");
+PrintLine();
+PrintRow($"{novoCliente.Nome}", $"{novoCliente.DataNascimento:dd/MM/yyyy}", $"{novoCliente.EstadoCivil}", $"{novoCliente.Cpf}");
+PrintRow($"", $"", $"", $"");
+PrintLine();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+PrintLine();
+PrintRow("Nome", "Data Nascimento", "Estado Civil", "CPF");
+PrintLine();
+PrintRow($"{novoCliente.Nome}", $"{novoCliente.DataNascimento:dd/MM/yyyy}", $"{novoCliente.EstadoCivil}", $"{novoCliente.Cpf}");
+PrintRow($"", $"", $"", $"");
+PrintLine();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Red;
+
+PrintLine();
+PrintRow("Nome", "Data Nascimento", "Estado Civil", "CPF");
+PrintLine();
+PrintRow($"{novoCliente.Nome}", $"{novoCliente.DataNascimento:dd/MM/yyyy}", $"{novoCliente.EstadoCivil}", $"{novoCliente.Cpf}");
+Console.BackgroundColor = ConsoleColor.DarkCyan;
+PrintRow($"", $"", $"", $"");
+Console.BackgroundColor = ConsoleColor.Red;
+PrintLine();
+Console.ReadLine();
+
+
+static void PrintLine()
+{
+    Console.WriteLine(new string('-', 73));
+}
+
+static void PrintRow(params string[] columns)
+{
+    int width = (73 - columns.Length) / columns.Length;
+    string row = "|";
+
+    foreach (string column in columns)
+    {
+        row += AlignCentre(column, width) + "|";
+    }
+
+    Console.WriteLine(row);
+}
+
+static string AlignCentre(string text, int width)
+{
+    text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+    if (string.IsNullOrEmpty(text))
+    {
+        return new string(' ', width);
+    }
+    else
+    {
+        return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+    }
+}
+
+public struct DadosCliente
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public EstadoCivil EstadoCivil { get; set; }
+    public string Cpf { get; set; }
+}
+
+public struct DadosEmpresa
+{
+    public string Nome { get; set; }
+    public long Cnpj { get; set; }
+}
+
+public struct DadosEmpresaBolsa
+{
+    public string Nome { get; set; }
+    public string Codigo { get; set; }
+}
+
+public struct RepositorioCliente
+{
+    public bool Salvar(DadosCliente cliente)
+    {
+        return false;
+    }
+
+    public void Salvar(List<DadosCliente> cliente)
+    {
+    }
+}
+
+public enum EstadoCivil
+{
+    Solteiro,
+    Casado,
+    Divorciado,
+    Viuvo,
+}
---
+++ AulaExtraListasDicionario/Program.cs
@@ -0,0 +1,218 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("===== Aula Extra ======");
+// Dúvidas sobre arquivo e manipulação de arquivos
+// Dúvidas sobre array,
+
+// SE
+// SENAO
+// SENAO SE
+// ENQUANTO
+
+// ATRAVESSAR()    <- ACAO
+// AGUARDAR()      <- ACAO
+// OLHARRUA()      <- ACAO
+// OLHARSEMAFORO() <- ACAO
+// APERTARBOTAO()  <- ACAO
+// SEMAFORO        <- OBJETO / ESTADO
+// MOVIMENTORUA    <- OBJETO / ESTADO
+
+
+// SEMAFORO <= OLHARSEMAFORO()
+// SE SEMAFORO ESTIVER VERDE  <-- IF
+//    MOVIMENTORUA <= OLHARRUA()
+//    (ENQUANTO MOVIMENTORUA IGUAL MOVIMENTADA) <-- WHILE
+//         AGUARDAR()
+//         MOVIMENTORUA <= OLHARRUA()
+//    ATRAVESSAR()
+// SENAO                <-- ELSE
+//    APERTARBOTAO()
+//    AGUARDAR()
+//    SEMAFORO <= OLHARSEMAFORO()
+//    
+
+// =================================================
+// SEMAFORO <= OLHARSEMAFORO()
+// (ENQUANTO SEMAFORO IGUAL VERMELHO)
+//      SEMAFORO <= OLHARSEMAFORO()
+//      AGUARDAR()
+//  MOVIMENTORUA <= OLHARRUA()
+//  (ENQUANTO MOVIMENTORUA IGUAL MOVIMENTADA) <-- WHILE
+//      AGUARDAR()
+//      MOVIMENTORUA <= OLHARRUA()
+//   ATRAVESSAR()
+
+// =================================
+// CONDICIONAIS
+// if
+// else
+// else if
+// LAÇOS DE REPETIÇÃO
+// foreach <- coleção de dados / finito
+// for
+// while
+// do while
+
+for (; ;)
+{
+    Console.WriteLine("Olá, Mundo - for sem condição!");
+    var input = Console.ReadKey();
+    if (input.Key == ConsoleKey.Enter)
+        break;
+}
+
+while (true)
+{
+    Console.WriteLine("Olá, Mundo - while!");
+    var input = Console.ReadKey();
+    if (input.Key == ConsoleKey.Enter)
+        break;
+}
+
+do
+{
+    Console.WriteLine("Olá, Mundo - do while!");
+    var input = Console.ReadKey();
+    if (input.Key == ConsoleKey.Enter)
+        break;
+} while (true);
+
+//for (; ; ) // <- condicionais
+//{
+//    // sem condição mano
+//}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Olá, Mundo - for com {i}!");
+}
+
+/// ==============================
+/// ARRAY
+
+string[] arrStr;
+int[] arrInt;
+char[] arrChar;
+var ex = "teste".Length;
+
+//foreach (var item in "Heber")
+//{
+
+//}
+
+//var arrayAlunos = new string[] { "Heber", "" };
+//string[] arrBlaBla = string[10];
+string[] arrayProfessores = new string[10];
+
+string[] arrayAlunos = { "Heber", "Mari", "Lucas" };
+
+foreach (var aluno in arrayAlunos)
+{
+    Console.WriteLine(aluno);
+}
+
+//==================================
+// SPLIT
+
+var nome = "Heber Henrique da Silva";
+var arrNome = nome.Split();
+Console.Clear();
+foreach (var nomeD in arrNome)
+{
+    Console.WriteLine(nomeD);
+}
+
+Console.ReadKey();
+
+Console.Clear();
+
+Console.WriteLine("quantos patinhos foram passear?");
+var qtdPatinhos = Convert.ToInt32(Console.ReadLine());
+
+StringBuilder sb = new StringBuilder();
+
+for (int i = qtdPatinhos; i > 0; i--)
+{
+    if (i == 1)
+        sb.AppendLine($" {i} foi passear alem das montanhas para brincar!");
+    else
+        sb.AppendLine($" {i} foram passear alem das montanhas para brincar!");
+
+    sb.AppendLine("A mamae gritou qua qua qua qua");
+
+    if (i == 1)
+        sb.AppendLine($"mas nenhum patinho voltou de lá");
+    else if (i == 2)
+        sb.AppendLine($"mas só 1 patinho voltou de lá");
+    else
+        sb.AppendLine($"mas só {i - 1} patinhos voltaram de lá");
+}
+
+Console.WriteLine(sb.ToString());
+
+Console.Clear();
+
+var listaVip = new List<string>();
+
+listaVip.Add("Nat Fontanezi");
+listaVip.Add("Marcella Roverato");
+
+
+
+foreach (var item in listaVip)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("************");
+
+var nomeLista = listaVip[0].Split();
+
+foreach (var item in nomeLista)
+{
+    Console.WriteLine(item);
+    listaVip.Add(item);
+}
+
+Console.WriteLine("************");
+
+foreach (var item in listaVip)
+{
+    Console.WriteLine(item);
+}
+
+//=========================
+Console.Clear();
+var dicGrupoAlunos = new Dictionary<string, List<string>>();
+var grupo1 = new List<string>();
+grupo1.Add("Augustinho");
+grupo1.Add("Lucas");
+grupo1.Add("Lugan");
+var grupo2 = new List<string>();
+grupo2.Add("Marcella");
+grupo2.Add("Matheus");
+grupo2.Add("Robs");
+dicGrupoAlunos.Add("Marvel", grupo1);
+dicGrupoAlunos.Add("DC Comics", grupo2);
+
+foreach (var grupo in dicGrupoAlunos)
+{
+    Console.WriteLine(grupo.Key);
+    foreach (var aluno in grupo.Value)
+    {
+        Console.WriteLine(aluno);
+    }
+}
+
+
+
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("***********");
+
+
+foreach (var aluno in dicGrupoAlunos["Marvel"])
+{
+    Console.WriteLine(aluno);
+}
